df1b6a956eec604f8ce3adb0153df717c17671cf
Andrei Warkentin <andrei.warkentin@intel.com>
2023-03-10 15:38:53 -0600

    FdtBusDxe: the beginning
    set -e
    echo `pwd`
    . edksetup.sh
    export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
    export GCC5_RISCV64_PREFIX=riscv64-linux-gnu-
    
    build -a AARCH64 -t GCC5 -p SseUefiPkg/SseUefiPkg.dsc -b DEBUG
    build -a RISCV64 -t GCC5 -p SseUefiPkg/SseUefiPkg.dsc -b DEBUG
    
    Signed-off-by: Andrei Warkentin <andrei.warkentin@intel.com>

---
+++ Drivers/FdtBusDxe/Entry.c
@@ -0,0 +1,28 @@
+/** @file
+
+    Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+**/
+
+#include <Library/BaseLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
+#include <Library/UefiLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiDriverEntryPoint.h>
+
+EFI_STATUS
+EFIAPI
+EntryPoint (
+  IN  EFI_HANDLE        ImageHandle,
+  IN  EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  return EFI_SUCCESS;
+}

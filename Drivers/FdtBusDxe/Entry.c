06fa52b012f576da7cf3330016386f65db0a3c2d
Andrei Warkentin <andrei.warkentin@intel.com>
2023-03-16 00:45:39 -0500

    FdtBusDxe: fix license
    This wasn't ever meant to be GPL. I just accidentally pasted
    a header from somewhere else.
    
    We want this to make it into edk2. BSD.
    
    Signed-off-by: Andrei Warkentin <andrei.warkentin@intel.com>

--- Drivers/FdtBusDxe/Entry.c
+++ Drivers/FdtBusDxe/Entry.c
@@ -2,10 +2,7 @@
 
     Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+    SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
